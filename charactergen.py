f9a43cc452d3bc47b4291c700cf1073640f3117e
Kevin Burkeland <kevin@burke.land>
2019-11-09 13:08:02 -0800

    Update charactergen.py
    Fixed formatting of print command

--- charactergen.py
+++ charactergen.py
@@ -2,12 +2,12 @@
 #rev 0.01
 #writen by Kevin Burkeland
 import dice, mod, cclass
-print "Welcome to revision .01 of Kevin Burkelands Pathfinder Character Gen script"
-print "Pick your stat gen style:"
-print "1. Generous: 4d6 drops the lowest and gives you one 18 to start with"
-print "2. Standard: roll 4d6 drops the lowest"
-print "3. Rough: 3d6, thats all you get"
-print "4. Old school: 3d6 down the line, you don't get to choose which role goes where"
+print('Welcome to revision .01 of Kevin Burkelands Pathfinder Character Gen script')
+print('Pick your stat gen style:')
+print('1. Generous: 4d6 drops the lowest and gives you one 18 to start with')
+print('2. Standard: roll 4d6 drops the lowest')
+print('3. Rough: 3d6, thats all you get')
+print("4. Old school: 3d6 down the line, you don't get to choose which role goes where")
 #restart if they don't answer properly
 while True:
 	#gets the choice var
@@ -24,7 +24,7 @@ while True:
 			stats.append(stat)
 		#adds the 18
 		stats.append(18)
-		print stats
+		print(stats)
 		break
 	if choice == 2:
 		#initialize the stats array
@@ -34,7 +34,7 @@ while True:
 			rolls = dice.roll_d6(4)
 			stat = sum(rolls) - min(rolls)
 			stats.append(stat)
-		print stats
+		print(stats)
 		break
 	if choice == 3:
 		#initialiaze the stats array
@@ -44,7 +44,7 @@ while True:
 			rolls = dice.roll_d6(3)
 			stat = sum(rolls)
 			stats.append(stat)
-		print stats
+		print(stats)
 		break
 	if choice == 4:
 		attrib = ["str","dex","con","int","wis","cha"]
@@ -54,9 +54,9 @@ while True:
                         stat = sum(rolls)
                         stats.append(stat)
 		attribs = dict(zip(attrib, stats))
-		print attribs		
+		print(attribs)
 		break
-	print "please try again"
+	print("please try again")
 #start of attributes
 attrib = ["str","dex","con","int","wis","cha"]
 if 'attribs' not in locals():
@@ -67,11 +67,11 @@ if 'attribs' not in locals():
 			if stat in stats:
 				attribs[_] = stat
 				stats.remove(stat)
-				print stats
+				print(stats)
 				break
 			else:
-				print "you didn't roll that"
-				print stats
+				print("you didn't roll that")
+				print(stats)
 #end of first attributes section
 amods = mod.ablemod(attribs)
-print amods
+print(amods)
