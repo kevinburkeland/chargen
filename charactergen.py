e6c87914e23c7a11c91c7103ab0bb218ef782385
Kevin Burkeland <kevin@burke.land>
2019-11-09 17:35:00 -0800

    Update charactergen.py
    fixed quotes

--- charactergen.py
+++ charactergen.py
@@ -2,11 +2,11 @@
 #rev 0.01
 #writen by Kevin Burkeland
 import dice, mod, cclass
-print('Welcome to revision .01 of Kevin Burkelands Pathfinder Character Gen script')
-print('Pick your stat gen style:')
-print('1. Generous: 4d6 drops the lowest and gives you one 18 to start with')
-print('2. Standard: roll 4d6 drops the lowest')
-print('3. Rough: 3d6, thats all you get')
+print("Welcome to revision .01 of Kevin Burkelands Pathfinder Character Gen script")
+print("Pick your stat gen style:")
+print("1. Generous: 4d6 drops the lowest and gives you one 18 to start with")
+print("2. Standard: roll 4d6 drops the lowest")
+print("3. Rough: 3d6, thats all you get")
 print("4. Old school: 3d6 down the line, you don't get to choose which role goes where")
 #restart if they don't answer properly
 while True:
